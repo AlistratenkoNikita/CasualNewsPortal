401a63ac4c8232ce923498251806afd8fdf95d07
steelheroe@gmail.com <steelheroe@gmail.com>
2019-05-07 14:41:46 +0300

    initial project commit

---
+++ src/main/java/ua/com/alistratenko/petproject/AlistratenkoPetProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alistratenko.petproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlistratenkoPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlistratenkoPetProjectApplication.class, args);
+	}
+
+}
