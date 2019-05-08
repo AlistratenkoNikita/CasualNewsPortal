20a831b1bd7d8937e119a7e8cccf6adcb9908a43
steelheroe@gmail.com <steelheroe@gmail.com>
2019-05-08 15:25:43 +0300

    updated configuration, added mongodb support

--- src/main/java/ua/com/alistratenko/petproject/AlistratenkoPetProjectApplication.java
+++ src/main/java/ua/com/alistratenko/petproject/AlistratenkoPetProjectApplication.java
@@ -2,12 +2,19 @@ package ua.com.alistratenko.petproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AlistratenkoPetProjectApplication {
+public class AlistratenkoPetProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlistratenkoPetProjectApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AlistratenkoPetProjectApplication.class);
 	}
 
 }
---
+++ src/main/java/ua/com/alistratenko/petproject/MvcConfiguration.java
@@ -0,0 +1,28 @@
+package ua.com.alistratenko.petproject;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".html");
+		return resolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
